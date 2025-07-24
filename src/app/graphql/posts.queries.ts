debc9a7af0bac7ffbff6b102f7331e810ea9f361
FavoMeister <93888759+FavoMeister@users.noreply.github.com>
2025-07-23 22:21:56 -0400

    Crating fragment in the project

--- src/app/graphql/posts.queries.ts
+++ src/app/graphql/posts.queries.ts
@@ -1,22 +1,20 @@
 import { gql } from 'apollo-angular';
 import { GetPost, GetPosts, GetPostVariables } from './posts.types';
+import { POST_TABLE_FIELDS_FRAGMENT } from './posts.fragments';
 
 export const GET_POSTS = gql<GetPosts, unknown>`
   query MyQuery {
     allPosts {
-      id
-      title
-      views
+      ...PostTableFields
     }
   }
+  ${POST_TABLE_FIELDS_FRAGMENT}
 `;
 
 export const GET_POST = gql <GetPost, GetPostVariables>`
   query MyQuery($id: ID!) {
     Post(id: $id) {
-      id
-      title
-      views
+      ...PostTableFields
       comment
     }
   }
